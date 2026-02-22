12ce68ce05e21c4692516c48823d7523a55b655a
yonassolomon <yonaisol21@gmail.com>
2026-02-21 16:29:42 -1000

    fixing some syntax error

--- backend/api/models.py
+++ backend/api/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     
 class Like(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='likes')
-    post=models.ForeignKey(post,on_delete=models.CASCADE,related_name='likes')
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name='likes')
     created_at=models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -28,7 +28,7 @@ class Like(models.Model):
 
 class Comment(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='comments')
-    post=models.ForeignKey(post,on_delete=models.CASCADE,related_name='comments')
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
     content=models.TextField(max_length=300)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
